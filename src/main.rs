213b83f83d95381c961f28cc17b0b3671dc20c25
Jaime <jaimegonzalezfabregas@gmail.com>
2024-10-28 22:19:06 +0100

    some fixes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![feature(generic_arg_infer)]
 
-use ia_engine::{
+use jaime::{
     simd_arr::dense_simd::DenseSimd,
     trainer::{default_param_translator, DataPoint, Trainer},
 };
@@ -15,14 +15,15 @@ fn main() {
         output: [-200.],
     }];
 
-    let mut trainer: Trainer<_, _, _, _, DenseSimd<_>, _, _, _> = Trainer::new(
-        direct,
-        direct,
-        default_param_translator,
-        (),
-    );
+    let mut trainer: Trainer<_, _, _, _, DenseSimd<_>, _, _, _> =
+        Trainer::new_dense(direct, direct, default_param_translator, ());
 
-    while trainer.train_step(&dataset) {
+    while trainer.train_step_asintotic_search::<false, false, _, _>(
+        &dataset,
+        &dataset,
+        dataset.len(),
+        dataset.len(),
+    ) {
         println!("{:?}", trainer.get_model_params());
     }
     println!("{:?}", trainer.get_model_params());
